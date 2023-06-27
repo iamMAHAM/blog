5d092f931581b1f5262fb8f2c81b25291f86fdd4
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-06-27 12:11:43 +0000

    terminer page inscription et connexion

---
+++ src/component/login.jsx
@@ -0,0 +1,10 @@
+export default function Login(){
+    return(
+        <form action="" className="form">
+            <h1>CONNEXION</h1>
+            <input type="text" placeholder="Email" /><br />
+            <input type="password" placeholder="Mot de passe" /><br />
+            <button>CONNECTER</button>
+        </form>
+    )
+}
